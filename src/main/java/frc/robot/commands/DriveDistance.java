5e805c598fca63e8ebb855a56681164f678cad79
Sam <samelite2008@gmail.com>
2022-11-10 19:33:40 -0800

    Simulation parameters

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -1,4 +1,3 @@
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -64,7 +63,7 @@ public class DriveDistance extends CommandBase {
   public void execute() {
     m_drive.tankDrive(
       //Left PID Controller Calculations
-      m_leftPIDcontroller.calculate(m_drive.getLeftDistanceInch(), m_distance), //Could possible use the MathUtil.clamp() if we need to keep the speed down
+      m_leftPIDcontroller.calculate(m_drive.getLeftDistanceInch(), m_distance), // Could possible use the MathUtil.clamp() if we need to keep the speed down
       //Right PID Controller Calculations
       m_rightPIDcontroller.calculate(m_drive.getRightDistanceInch(), m_distance),
       // Squared Inputs
